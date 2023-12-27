3cc2aff7ff631f2edc6ac7aea3c110400cac820a
elenakrittik <dev.elenakrittik@gmail.com>
2023-12-27 20:27:48 +0300

    Go!

--- Main.hs
+++ Main.hs
@@ -1,3 +1,6 @@
+newtype Html = Html String
+newtype Document = Document Html
+
 head_ :: String -> String
 head_ content = "<head>" <> content <> "</head>"
 
@@ -6,6 +9,12 @@ title_ content = "<title>" <> content <> "</title>"
 
 body_ :: String -> String
 body_ content = "<body>" <> content <> "</body>"
+
+h1_ :: String -> String
+h1_ content = "<h1>" <> content <> "</h1>"
+
+p_ :: String -> String
+p_ content = "<p>" <> content <> "</p>"
 
 html_ :: String -> String
 html_ content = "<html>" <> content <> "</html>"
@@ -19,7 +28,10 @@ makeHtml title body = doctype_ <>
     head_ (
       title_ title
     ) <>
-    body_ body
+    body_ (
+      h1_ title <>
+      p_ body
+    )
   )
 
 main = putStrLn (makeHtml "Hspage" "Hello?")
